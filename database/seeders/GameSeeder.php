d9d95749f259ba4559af6cc120edc1b24343ab0f
azrahel2020 <79024820+azrahel2020@users.noreply.github.com>
2024-06-25 09:12:47 -0500

    Update GameSeeder.php

--- database/seeders/GameSeeder.php
+++ database/seeders/GameSeeder.php
@@ -19,8 +19,8 @@ class GameSeeder extends Seeder
             'date' => '2024-06-24 17:00:00',
             'stadium' => 'Levis Stadium',
             'quiniela_id' => 1,
-            'home_team_id' => 14,
-            'away_team_id' => 15,
+            'home_team_id' => 2,
+            'away_team_id' => 4,
 
         ]);
         Game::create([
@@ -37,8 +37,8 @@ class GameSeeder extends Seeder
             'date' => '2024-06-25 17:00:00',
             'stadium' => 'Levis Stadium',
             'quiniela_id' => 1,
-            'home_team_id' => 2,
-            'away_team_id' => 4,
+            'home_team_id' => 6,
+            'away_team_id' => 8,
 
         ]);
         Game::create([
@@ -46,137 +46,14 @@ class GameSeeder extends Seeder
             'date' => '2024-06-25 20:00:00',
             'stadium' => 'Levis Stadium',
             'quiniela_id' => 1,
-            'home_team_id' => 3,
-            'away_team_id' => 1,
+            'home_team_id' => 7,
+            'away_team_id' => 5,
 
         ]);
 
-        //eurocopa
-        Game::create([
-
-            'date' => '2024-06-24 14:00:00',
-            'stadium' => 'Levis Stadium',
-            'quiniela_id' => 2,
-            'home_team_id' => 23,
-            'away_team_id' => 24,
-
-        ]);
-        Game::create([
-
-            'date' => '2024-06-24 14:00:00',
-            'stadium' => 'Levis Stadium',
-            'quiniela_id' => 2,
-            'home_team_id' => 25,
-            'away_team_id' => 26,
-
-        ]);
+        
 
 
-
-
-
-
-        /* Game::create([
-
-            'date' => '2024-06-22 17:00:00',
-            'stadium' => 'Levis Stadium',
-            'quiniela_id' => 1,
-            'home_team_id' => 6,
-            'away_team_id' => 7,
-
-        ]);
-        Game::create([
-
-            'date' => '2024-06-22 20:00:00',
-            'stadium' => 'NRG stadium',
-            'quiniela_id' => 1,
-            'home_team_id' => 5,
-            'away_team_id' => 8,
-
-        ]);
-        Game::create([
-
-            'date' => '2024-06-23 17:00:00',
-            'stadium' => 'At & T stadium',
-            'quiniela_id' => 1,
-            'home_team_id' => 9,
-            'away_team_id' => 12,
-
-        ]);
-        Game::create([
-
-            'date' => '2024-06-23 20:00:00',
-            'stadium' => 'hard Rock stadium',
-            'quiniela_id' => 1,
-            'home_team_id' => 10,
-            'away_team_id' => 11,
-
-        ]);
-        Game::create([
-
-            'date' => '2024-06-24 17:00:00',
-            'stadium' => 'hard Rock stadium',
-            'quiniela_id' => 1,
-            'home_team_id' => 14,
-            'away_team_id' => 15,
-
-        ]);
-        Game::create([
-
-            'date' => '2024-06-24 20:00:00',
-            'stadium' => 'hard Rock stadium',
-            'quiniela_id' => 1,
-            'home_team_id' => 13,
-            'away_team_id' => 16,
-
-        ]);
-
-        //eurocopa
-        Game::create([
-
-            'date' => '2024-06-22 14:00:00',
-            'stadium' => 'Levis Stadium',
-            'quiniela_id' => 2,
-            'home_team_id' => 17,
-            'away_team_id' => 18,
-
-        ]);
-        Game::create([
-
-            'date' => '2024-06-23 14:00:00',
-            'stadium' => 'Levis Stadium',
-            'quiniela_id' => 2,
-            'home_team_id' => 19,
-            'away_team_id' => 20,
-
-        ]);
-        Game::create([
-
-            'date' => '2024-06-23 14:00:00',
-            'stadium' => 'Levis Stadium',
-            'quiniela_id' => 2,
-            'home_team_id' => 21,
-            'away_team_id' => 22,
-
-        ]);
-        Game::create([
-
-            'date' => '2024-06-24 14:00:00',
-            'stadium' => 'Levis Stadium',
-            'quiniela_id' => 2,
-            'home_team_id' => 23,
-            'away_team_id' => 24,
-
-        ]);
-        Game::create([
-
-            'date' => '2024-06-24 14:00:00',
-            'stadium' => 'Levis Stadium',
-            'quiniela_id' => 2,
-            'home_team_id' => 25,
-            'away_team_id' => 26,
-
-        ]); */
 
         
     }
